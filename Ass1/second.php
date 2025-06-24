a2cde6e0f68cbc3fb9fb34c8653e88d98737ed1e
DNishadini <Dinushikaherath150@gmail.com>
2025-06-24 22:34:02 +0530

    PHP Course-2

--- Ass1/second.php
+++ Ass1/second.php
@@ -1,5 +1,5 @@
 <?php
-    $str = "Hello & Welcome to Simplilearn";
+   /* $str = "Hello & Welcome to Simplilearn";
     $num = 5;
     $float = 7.6;
 
@@ -12,4 +12,66 @@
     //output
     //array(4) { [0]=> string(2) "HP" [1]=> string(6) "Lenovo" [2]=> string(4) "Asus" [3]=> string(4) "Dell" }
     echo var_dump($laptop_brand[1]);
+
+    define("laptops","HP,Lenovo and Asus");
+    echo laptops;
+
+    define("laptops", ["HP","Lenovo and","Asus"]);
+    echo laptops[2];
+
+    $var = 5;
+    if($var > 7){
+        echo $var;
+
+    }else{
+        echo "The condition did not meet";
+
+    }
+
+    $var = 6;
+    if($var % 2 == 0 && $var % 3 == 0){
+        echo "Divisible by both";
+    }elseif($var % 2 == 0){
+        echo "Divisible by 2";
+    }elseif($var % 3 == 0){
+        echo "Divisible by 3";
+    }else{
+        echo "The condition did not meet";
+    }
+
+    $level = 3;
+    switch($level){
+        case 1:
+            echo "You are playing at EASY level";
+            break;
+        case 2:
+            echo "You are playing at MODERATE level";
+            break;
+        case 3:
+            echo "You are playing at HARD level";
+            break;
+        case 4:
+            echo "You are playing at EXTREME level";
+            break;
+        default:
+            echo "Invalid Input";
+    }
+
+    $var = 2;
+    while($var <= 10){
+        echo "Number: $var <br>";
+        $var++;
+    }
+
+    $var = 11;
+    do{
+        echo "Number: $var <br>";
+        $var++;
+    }while($var <= 10)*/
+
+    for($var = 1;$var <= 6;$var++){
+        echo "Number: $var <br>";
+    }
+
+
 ?>
