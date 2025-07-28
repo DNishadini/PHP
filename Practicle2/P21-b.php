52d5aa1d545b43c53c006bb8c9879beed34adda8
DNishadini <Dinushikaherath150@gmail.com>
2025-07-28 23:23:09 +0530

    Create P21-b.php

---
+++ Practicle2/P21-b.php
@@ -0,0 +1,13 @@
+<html>
+    <head></head>
+    <body>
+        <?php
+        $user_name="cspart3";
+        if($user_name=="cspart3"){
+            echo "login successful";
+        }else {
+            echo "You are unauthorized person";
+        }
+        ?>
+    </body>
+</html>
