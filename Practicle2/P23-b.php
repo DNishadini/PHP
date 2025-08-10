2ad70884cdc962bfdcb40e4fc9ad7c73477e6692
DNishadini <Dinushikaherath150@gmail.com>
2025-08-10 20:41:34 +0530

    Question P23-a & p23-b

---
+++ Practicle2/P23-b.php
@@ -0,0 +1,10 @@
+<?php
+    $start=10;
+    for($n=$start;$n>=0;$n--){
+        if($n % 2 == 0){
+            echo "$n is even<br>";
+        }else{
+            echo "$n is odd<br>"; 
+        }
+    }
+?>
