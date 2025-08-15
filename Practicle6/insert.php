6beeb4ed4ea22a98812fa9965b3f621c022a9431
DNishadini <Dinushikaherath150@gmail.com>
2025-08-15 13:46:42 +0530

    add

---
+++ Practicle6/insert.php
@@ -0,0 +1,21 @@
+<?php
+    include("conn.php");
+    echo "<br>";
+
+
+    $conn->select_db("jobseekers");
+
+    $sql="INSERT INTO personal_info (title,name,gender,dateofbirths) VALUES
+    ('Ms.', 'K. Sedara',  'F', '1980-11-02'),
+    ('Mr.',  'S. Alwis',   'M', '1969-03-03'),
+    ('Prof.', 'A. Perera', 'F', '1982-04-14')";
+
+    if($conn->query($sql)==true){
+        echo "Records inserted successfully";
+    }else{
+        echo "Error inserting records ".$conn->error;
+    }
+
+    $conn->close();
+
+?>
