a6ef188b7b928d0440e5ea5d63fcef59f61eee3f
DNishadini <Dinushikaherath150@gmail.com>
2025-08-23 22:02:43 +0530

    xml part 1

--- Practicle6/insert.php
+++ Practicle6/insert.php
@@ -5,10 +5,10 @@
 
     $conn->select_db("jobseekers");
 
-    $sql="INSERT INTO personal_info (title,name,gender,dateofbirths) VALUES
+    $sql="INSERT INTO personal_info (title,name,gender,dateofbirths) VALUES (
     ('Ms.', 'K. Sedara',  'F', '1980-11-02'),
     ('Mr.',  'S. Alwis',   'M', '1969-03-03'),
-    ('Prof.', 'A. Perera', 'F', '1982-04-14')";
+    ('Prof.', 'A. Perera', 'F', '1982-04-14'))";
 
     if($conn->query($sql)==true){
         echo "Records inserted successfully";
